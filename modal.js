09c58a78688bad728afd56272418f1a95b9b907f
Upshur06 <mupshur2005@gmail.com>
2020-08-28 22:23:13 -0400

    buttons and functions

--- modal.js
+++ modal.js
@@ -1,1 +1,11 @@
+let modalBtn = document.querySelector(".modal-btn");
+let modalContainer = document.querySelector(".modal-container");
+let closeBtn = document.querySelector(".close-btn");
 
+modalBtn.addEventListener("click", function(){
+  
+});
+
+closeBtn.addEventListener("click", function(){
+
+})
